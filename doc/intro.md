f95cda1502272de16586c51cb3409e77977ef060
Stefan Boos <kontakt@boos.systems>
2022-09-22 05:35:19 +0200

    initial leiningen project with .gitignore from gitignore.io

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kata-tribonacci-sequence
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kata_tribonacci_sequence/core_test.clj
@@ -0,0 +1,7 @@
+(ns kata-tribonacci-sequence.core-test
+  (:require [clojure.test :refer :all]
+            [kata-tribonacci-sequence.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
