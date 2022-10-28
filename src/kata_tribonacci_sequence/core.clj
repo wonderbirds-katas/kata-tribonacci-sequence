9a9b7583bce7ebb0388fa539ab334fb2df9ee4cc
Stefan Boos <kontakt@boos.systems>
2022-10-28 05:46:19 +0200

    refactor: simplify algorithm

--- src/kata_tribonacci_sequence/core.clj
+++ src/kata_tribonacci_sequence/core.clj
@@ -1,11 +1,7 @@
 (ns kata-tribonacci-sequence.core)
 
 (defn trib [[a b c]]
-  (let [sum (+ a b c)]
-    (lazy-seq (cons sum (trib [b c sum])))))
-
-(defn prefix-trib-with [signature]
-  (lazy-seq (concat signature (trib signature))))
+  (lazy-seq (cons a (trib [b c (+ a b c)]))))
 
 (defn tribonacci [signature n]
-       (take n (prefix-trib-with signature)))
+       (take n (trib signature)))
