8a646b6aa257f7b19ce898dd56407e57973ee378
Stefan Boos <kontakt@boos.systems>
2022-10-28 05:42:43 +0200

    feat: completed tribonacci sequence

--- src/kata_tribonacci_sequence/core.clj
+++ src/kata_tribonacci_sequence/core.clj
@@ -1,7 +1,11 @@
 (ns kata-tribonacci-sequence.core)
 
-(defn trib [signature]
-      (lazy-seq (concat signature (trib signature))))
+(defn trib [[a b c]]
+  (let [sum (+ a b c)]
+    (lazy-seq (cons sum (trib [b c sum])))))
+
+(defn prefix-trib-with [signature]
+  (lazy-seq (concat signature (trib signature))))
 
 (defn tribonacci [signature n]
-       (take n (trib signature)))
+       (take n (prefix-trib-with signature)))
